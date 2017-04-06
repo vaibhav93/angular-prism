a7c088c2f1b64418fc1ce77af9c95f4b7b0c65c9
Vaibhav Bansal <vaibhav@agentdesks.com>
2017-04-07 01:08:30 +0530

    prism component

---
+++ angular-prism.ts
@@ -0,0 +1,5 @@
+//import { NgModule, ModuleWithProviders } from '@angular/core';
+//import { CommonModule } from '@angular/common';
+import { PrismComponent } from './src/prism.component';
+
+export * from './src/prism.component';
